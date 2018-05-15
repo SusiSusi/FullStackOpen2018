f1598af66dcea95438cdb26289dd9a354d1325cf
SusiSusi <suvi.sipila@helsinki.fi>
2018-05-15 14:03:33 +0300

    1.13 tehty

--- Osa1/anekdootit/src/index.js
+++ Osa1/anekdootit/src/index.js
@@ -7,11 +7,26 @@ const Button = ({ text, handleClick }) => (
     </button>
 )
 
+const getRandomNumber = () => (
+    Math.floor((Math.random() * anecdotes.length))
+)
+
+const addNewVote = (votes, index) => {
+    const copyOfVotes = [...votes]
+    if (copyOfVotes[index]) {
+        copyOfVotes[index] += 1
+    } else {
+        copyOfVotes[index] = 1
+    }
+    return copyOfVotes
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      selected: 0
+      selected: 0,
+      votes: [0]
     }
   }
 
@@ -23,11 +38,31 @@ class App extends React.Component {
         }  
     }
 
+    onClickVote = (anecdote) => {      
+        const copyVotes = addNewVote(this.state.votes, anecdote)
+        return () => {
+            this.setState({
+                votes: copyVotes
+            })
+       } 
+    }
+
+    countVotes = () => {
+        if (this.state.votes[this.state.selected]) {
+            return this.state.votes[this.state.selected]
+        }
+        return 0;
+    }
+
   render() {
     return (
       <div>
         {this.props.anecdotes[this.state.selected]}
+        <p> has {this.countVotes()} votes </p>
         <p></p>
+        <Button handleClick={this.onClickVote(this.state.selected)}
+        text={"Vote"}
+        />
         <Button handleClick={this.onClick()}
         text={"Next anecode"}
         />
@@ -44,10 +79,6 @@ const anecdotes = [
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
-
-const getRandomNumber = () => (
-    Math.floor((Math.random() * anecdotes.length))
-)
 
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
