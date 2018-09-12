295e800595a17a6566cc40539438b60a16f6287f
SusiSusi <suvi.sipila@helsinki.fi>
2018-09-12 14:47:04 +0300

    harkkatehtävät

--- Osa2/Osa3_projekti/index.js
+++ Osa2/Osa3_projekti/index.js
@@ -9,93 +9,93 @@ app.use(cors())
 app.use(express.static('build'))
 
 let notes = [
-    {
-      id: 1,
-      content: 'HTML on helppoa',
-      date: '2017-12-10T17:30:31.098Z',
-      important: true
-    },
-    {
-      id: 2,
-      content: 'Selain pystyy suorittamaan vain javascriptiä',
-      date: '2017-12-10T18:39:34.091Z',
-      important: false
-    },
-    {
-      id: 3,
-      content: 'HTTP-protokollan tärkeimmät metodit ovat GET ja POST',
-      date: '2017-12-10T19:20:14.298Z',
-      important: true
-    }
-  ]
+  {
+    id: 1,
+    content: 'HTML on helppoa',
+    date: '2017-12-10T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Selain pystyy suorittamaan vain javascriptiä',
+    date: '2017-12-10T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'HTTP-protokollan tärkeimmät metodit ovat GET ja POST',
+    date: '2017-12-10T19:20:14.298Z',
+    important: true
+  }
+]
 
 const formatNote = (note) => {
-    return {
-        content: note.content,
-        date: note.date,
-        important: note.important,
-        id: note._id
-    }
+  return {
+    content: note.content,
+    date: note.date,
+    important: note.important,
+    id: note._id
+  }
 }
-  
+
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (req, res) => {
-    Note
-        .find({})
-        .then(notes => {
-            res.json(notes.map(formatNote))
-        })
+  Note
+    .find({})
+    .then(notes => {
+      res.json(notes.map(formatNote))
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    Note
-        .findById(request.param.id)
-        .then(note => {
-            if(note) {
-                response.json(formatNote(note))
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => {
-            console.log(error)
-            response.status(400).send({ error: 'malformatted id' })
-        })
+  Note
+    .findById(request.param.id)
+    .then(note => {
+      if(note) {
+        response.json(formatNote(note))
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-    Note
-        .findByIdAndRemove(request.param.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => {
-            response.status(400).send({ error: 'malformatted id' })
-        })
+  Note
+    .findByIdAndRemove(request.param.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      response.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 const generateId = () => {
-    const maxId = notes.length > 0 ? notes.map(n => n.id).sort().reverse()[0] : 1
-    return maxId + 1
-  }
+  const maxId = notes.length > 0 ? notes.map(n => n.id).sort().reverse()[0] : 1
+  return maxId + 1
+}
 
 app.post('/api/notes', (request, response) => {
-    const body = request.body
-    
-    if (body.content === undefined) {
-        return response.status(404).json({ error: 'content missing' })
-    }
+  const body = request.body
+ 
+  if (body.content === undefined) {
+    return response.status(404).json({ error: 'content missing' })
+  }
 
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-        date: new Date()
-    })
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date()
+  })
 
-    note
+  note
     .save()
     .then(formatNote)
     .then(savedAndFormattedNote => {
@@ -104,25 +104,25 @@ app.post('/api/notes', (request, response) => {
 })
 
 app.put('/api/notes/:id', (request, response) => {
-    const body = request.body
+  const body = request.body
 
-    const note = {
-        content: body.content,
-        important: body.important
-    }
+  const note = {
+    content: body.content,
+    important: body.important
+  }
 
-    Note
-        .findByIdAndUpdate(request.params.id, note, { new: true })
-        .then(updatedNote => {
-            response.json(formatNote(updatedNote))
-        })
-        .catch(error => {
-            console.log(error)
-            response.status(400).send({ error: 'malformatted id' })
-        })
+  Note
+    .findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(formatNote(updatedNote))
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 const PORT = process.env.PORT || 3002   
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+  console.log(`Server running on port ${PORT}`)
+})
